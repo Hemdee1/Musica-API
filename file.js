988f47a317b7942f620ff954bede91551c8ce8b5
hemdee <sanusi5110@gmail.com>
2022-10-24 18:35:35 +0100

    added more playlists

--- file.js
+++ file.js
@@ -2,20 +2,25 @@ const newRelease = [
   {
     id: "new-1",
     artist: "Kizz Daniel",
+    duration: "2:56",
     title: "Cough (Odo)",
+    duration: "2:56",
     cover: "https://musica-api.up.railway.app/cover/cover_1.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_1.mp3",
   },
   {
     id: "new-2",
     artist: "Ayra Starr",
+    duration: "3:09",
     title: "Rush",
-    cover: "https://musica-api.up.railway.app/cover/cover_2.png",
+    duration: "3:05",
+    cover: "https://musica-api.up.railway.app/cover/cover_2.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_2.mp3",
   },
   {
     id: "new-3",
     artist: "Bella Shmurda",
+    duration: "2:09",
     title: "New Born Fela",
     cover: "https://musica-api.up.railway.app/cover/cover_3.jpg",
     audio: "https://musica-api.up.railway.app/audio/audio_3.mp3",
@@ -23,6 +28,7 @@ const newRelease = [
   {
     id: "new-4",
     artist: "Blaqbonez ft JAE5",
+    duration: "2:30",
     title: "Back In Uni",
     cover: "https://musica-api.up.railway.app/cover/cover_4.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_4.mp3",
@@ -30,6 +36,7 @@ const newRelease = [
   {
     id: "new-5",
     artist: "Pheelz ft Davido",
+    duration: "2:14",
     title: "Electricity",
     cover: "https://musica-api.up.railway.app/cover/cover_5.png",
     audio: "https://musica-api.up.railway.app/audio/audio_5.mp3",
@@ -37,6 +44,7 @@ const newRelease = [
   {
     id: "new-6",
     artist: "Oxlade",
+    duration: "2:27",
     title: "KU LO SA",
     cover: "https://musica-api.up.railway.app/cover/cover_6.webp",
     audio: "https://musica-api.up.railway.app/audio/audio_6.mp3",
@@ -44,6 +52,7 @@ const newRelease = [
   {
     id: "new-7",
     artist: "JAE5 ft Dave & BNXN",
+    duration: "3:24",
     title: "Propeller",
     cover: "https://musica-api.up.railway.app/cover/cover_7.png",
     audio: "https://musica-api.up.railway.app/audio/audio_7.mp3",
@@ -51,6 +60,7 @@ const newRelease = [
   {
     id: "new-8",
     artist: "Olamide",
+    duration: "2:35",
     title: "We Outside",
     cover: "https://musica-api.up.railway.app/cover/cover_8.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_8.mp3",
@@ -58,6 +68,7 @@ const newRelease = [
   {
     id: "new-9",
     artist: "Joeboy",
+    duration: "3:09",
     title: "Contour",
     cover: "https://musica-api.up.railway.app/cover/cover_9.jpg",
     audio: "https://musica-api.up.railway.app/audio/audio_9.mp3",
@@ -65,6 +76,7 @@ const newRelease = [
   {
     id: "new-10",
     artist: "Zinoleesky",
+    duration: "2:24",
     title: "Call Of Duty",
     cover: "https://musica-api.up.railway.app/cover/cover_10.jpg",
     audio: "https://musica-api.up.railway.app/audio/audio_10.mp3",
@@ -75,6 +87,7 @@ const popular = [
   {
     id: "popular-1",
     artist: "Asake",
+    duration: "2:19",
     title: "Terminator",
     cover: "https://musica-api.up.railway.app/cover/cover_11.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_11.mp3",
@@ -82,6 +95,7 @@ const popular = [
   {
     id: "popular-2",
     artist: "Magix ft Ayra Starr",
+    duration: "2:26",
     title: "Love Don't Cost A Dime",
     cover: "https://musica-api.up.railway.app/cover/cover_12.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_12.mp3",
@@ -89,6 +103,7 @@ const popular = [
   {
     id: "popular-3",
     artist: "Burna Boy",
+    duration: "2:52",
     title: "Last Last",
     cover: "https://musica-api.up.railway.app/cover/cover_13.jpg",
     audio: "https://musica-api.up.railway.app/audio/audio_13.mp3",
@@ -96,6 +111,7 @@ const popular = [
   {
     id: "popular-4",
     artist: "Kizz Daniel ft Tekno",
+    duration: "2:59",
     title: "Buga",
     cover: "https://musica-api.up.railway.app/cover/cover_14.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_14.mp3",
@@ -103,6 +119,7 @@ const popular = [
   {
     id: "popular-5",
     artist: "Steven ft Portable & TI Blaze",
+    duration: "3:22",
     title: "Ali Remix",
     cover: "https://musica-api.up.railway.app/cover/cover_15.png",
     audio: "https://musica-api.up.railway.app/audio/audio_15.mp3",
@@ -110,6 +127,7 @@ const popular = [
   {
     id: "popular-6",
     artist: "Asake ft Burna Boy",
+    duration: "3:30",
     title: "Sungba Remix",
     cover: "https://musica-api.up.railway.app/cover/cover_16.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_16.mp3",
@@ -117,6 +135,7 @@ const popular = [
   {
     id: "popular-7",
     artist: "Fireboy",
+    duration: "3:27",
     title: "Playboy",
     cover: "https://musica-api.up.railway.app/cover/cover_17.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_17.mp3",
@@ -124,6 +143,7 @@ const popular = [
   {
     id: "popular-8",
     artist: "BNXN ft Wande Coal",
+    duration: "3:33",
     title: "Kenkele",
     cover: "https://musica-api.up.railway.app/cover/cover_18.png",
     audio: "https://musica-api.up.railway.app/audio/audio_18.mp3",
@@ -131,6 +151,7 @@ const popular = [
   {
     id: "popular-9",
     artist: "Asake",
+    duration: "2:33",
     title: "PBUY",
     cover: "https://musica-api.up.railway.app/cover/cover_19.png",
     audio: "https://musica-api.up.railway.app/audio/audio_19.mp3",
@@ -138,6 +159,7 @@ const popular = [
   {
     id: "popular-10",
     artist: "Zinoleesky",
+    duration: "2:05",
     title: "Loving You",
     cover: "https://musica-api.up.railway.app/cover/cover_20.jpg",
     audio: "https://musica-api.up.railway.app/audio/audio_20.mp3",
@@ -148,6 +170,7 @@ const YBNLPlaylist = [
   {
     id: "ybnl-1",
     artist: "Olamide",
+    duration: "2:35",
     title: "We Outside",
     cover: "https://musica-api.up.railway.app/cover/cover_8.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_8.mp3",
@@ -155,6 +178,7 @@ const YBNLPlaylist = [
   {
     id: "ybnl-2",
     artist: "Asake",
+    duration: "2:33",
     title: "PBUY",
     cover: "https://musica-api.up.railway.app/cover/cover_19.png",
     audio: "https://musica-api.up.railway.app/audio/audio_19.mp3",
@@ -162,6 +186,7 @@ const YBNLPlaylist = [
   {
     id: "ybnl-3",
     artist: "Asake ft Burna Boy",
+    duration: "3:30",
     title: "Sungba Remix",
     cover: "https://musica-api.up.railway.app/cover/cover_16.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_16.mp3",
@@ -169,6 +194,7 @@ const YBNLPlaylist = [
   {
     id: "ybnl-4",
     artist: "Fireboy",
+    duration: "3:27",
     title: "Playboy",
     cover: "https://musica-api.up.railway.app/cover/cover_17.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_17.mp3",
@@ -176,6 +202,7 @@ const YBNLPlaylist = [
   {
     id: "ybnl-5",
     artist: "Asake",
+    duration: "2:19",
     title: "Terminator",
     cover: "https://musica-api.up.railway.app/cover/cover_11.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_11.mp3",
@@ -186,6 +213,7 @@ const zinoleeskyPlaylist = [
   {
     id: "zino-1",
     artist: "Zinoleesky",
+    duration: "2:24",
     title: "Call Of Duty",
     cover: "https://musica-api.up.railway.app/cover/cover_10.jpg",
     audio: "https://musica-api.up.railway.app/audio/audio_10.mp3",
@@ -193,6 +221,7 @@ const zinoleeskyPlaylist = [
   {
     id: "zino-2",
     artist: "Zinoleesky",
+    duration: "2:05",
     title: "Loving You",
     cover: "https://musica-api.up.railway.app/cover/cover_20.jpg",
     audio: "https://musica-api.up.railway.app/audio/audio_20.mp3",
@@ -203,6 +232,7 @@ const asakePlaylist = [
   {
     id: "asake-1",
     artist: "Asake",
+    duration: "2:33",
     title: "PBUY",
     cover: "https://musica-api.up.railway.app/cover/cover_19.png",
     audio: "https://musica-api.up.railway.app/audio/audio_19.mp3",
@@ -210,6 +240,7 @@ const asakePlaylist = [
   {
     id: "asake-2",
     artist: "Asake ft Burna Boy",
+    duration: "3:30",
     title: "Sungba Remix",
     cover: "https://musica-api.up.railway.app/cover/cover_16.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_16.mp3",
@@ -217,6 +248,7 @@ const asakePlaylist = [
   {
     id: "asake-3",
     artist: "Asake",
+    duration: "2:19",
     title: "Terminator",
     cover: "https://musica-api.up.railway.app/cover/cover_11.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_11.mp3",
@@ -227,16 +259,57 @@ const AyraStarrPlaylist = [
   {
     id: "ayra-1",
     artist: "Ayra Starr",
+    duration: "3:09",
     title: "Rush",
-    cover: "https://musica-api.up.railway.app/cover/cover_2.png",
+    cover: "https://musica-api.up.railway.app/cover/cover_2.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_2.mp3",
   },
   {
     id: "ayra-2",
     artist: "Magix ft Ayra Starr",
+    duration: "2:26",
     title: "Love Don't Cost A Dime",
     cover: "https://musica-api.up.railway.app/cover/cover_12.jpeg",
     audio: "https://musica-api.up.railway.app/audio/audio_12.mp3",
+  },
+];
+
+const kizzPlaylist = [
+  {
+    id: "new-1",
+    artist: "Kizz Daniel",
+    duration: "2:56",
+    title: "Cough (Odo)",
+    duration: "2:56",
+    cover: "https://musica-api.up.railway.app/cover/cover_1.jpeg",
+    audio: "https://musica-api.up.railway.app/audio/audio_1.mp3",
+  },
+  {
+    id: "popular-4",
+    artist: "Kizz Daniel ft Tekno",
+    duration: "2:59",
+    title: "Buga",
+    cover: "https://musica-api.up.railway.app/cover/cover_14.jpeg",
+    audio: "https://musica-api.up.railway.app/audio/audio_14.mp3",
+  },
+];
+
+const bujuPlaylist = [
+  {
+    id: "new-7",
+    artist: "JAE5 ft Dave & BNXN",
+    duration: "3:24",
+    title: "Propeller",
+    cover: "https://musica-api.up.railway.app/cover/cover_7.png",
+    audio: "https://musica-api.up.railway.app/audio/audio_7.mp3",
+  },
+  {
+    id: "popular-8",
+    artist: "BNXN ft Wande Coal",
+    duration: "3:33",
+    title: "Kenkele",
+    cover: "https://musica-api.up.railway.app/cover/cover_18.png",
+    audio: "https://musica-api.up.railway.app/audio/audio_18.mp3",
   },
 ];
 
@@ -245,26 +318,46 @@ const playlists = [
     id: "playlist-1",
     title: "YBNL Playlist",
     cover: "https://musica-api.up.railway.app/cover/play_cover_1.jpg",
+    info: "Yahoo Boy No Laptop Nation, popularly known as YBNL, is an independent record label founded by Olamide in 2012",
     files: YBNLPlaylist,
   },
   {
     id: "playlist-2",
     title: "Asake Playlist",
     cover: "https://musica-api.up.railway.app/cover/play_cover_2.png",
+    info: "Ahmed Ololade , known professionally as Asake, is a Nigerian singer and songwriter of Yoruba heritage who specialises in Afrobeats. He got signed to YBNL Nation and Empire Distribution.",
     files: asakePlaylist,
   },
   {
     id: "playlist-3",
     title: "Zinoleesky Playlist",
     cover: "https://musica-api.up.railway.app/cover/play_cover_3.jpg",
+    info: "Zinoleesky is a Nigerian singer and song-writer currently signed to Naira Marley record label, Marlian Music Imprint.",
     files: zinoleeskyPlaylist,
   },
   {
     id: "playlist-4",
     title: "Ayra Starr Playlist",
-    cover: "https://musica-api.up.railway.app/cover/play_cover_4.jpg",
+    cover: "https://musica-api.up.railway.app/cover/play_cover_4.jpeg",
+    info: "Oyinkansola Sarah Aderibigbe (born 14 June 2002), known professionally as Ayra Starr, is a Nigerian singer. Born in Cotonou, Benin, Ayra Starr began a fashion career at the age of 16 with Quove Model Management before deciding to pursue music.",
     files: AyraStarrPlaylist,
   },
+  {
+    id: "playlist-5",
+    title: "Kizz Daniel Playlist",
+    cover: "https://musica-api.up.railway.app/cover/play_cover_5.jpeg",
+    info: 'Oluwatobiloba Daniel Anidugbe, better known by his stage name Kizz Daniel, is a Nigerian singer and songwriter. He is best known for his singles "Woju" and "Yeba". He went by the stage name Kiss Daniel prior to changing it in May 2018.',
+    files: kizzPlaylist,
+  },
+  {
+    id: "playlist-6",
+    title: "BNXN (Buju) Playlist",
+    cover: "https://musica-api.up.railway.app/cover/play_cover_6.jpeg",
+    info: "Daniel Benson (born 14 May 1997), known professionally as Bnxn (pronounced as Benson) and formerly known as Buju, He is a Nigerian Afro-fusion singer, songwriter and record producer.",
+    files: bujuPlaylist,
+  },
 ];
+
+// https://musica-api.up.railway.app
 
 module.exports = { newRelease, popular, playlists };
