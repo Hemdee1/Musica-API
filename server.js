a8269573f363097364ca93b981bb4c35b88f97bc
hemdee <sanusi5110@gmail.com>
2022-11-25 00:06:00 +0100

    first commit in render branch

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 
 require("dotenv").config();
 
-const { newRelease, popular, playlists } = require("./file");
+// const { newRelease, popular, playlists } = require("./file");
+const { newRelease, popular, playlists } = require("./offlineFile");
 
 const app = express();
 
